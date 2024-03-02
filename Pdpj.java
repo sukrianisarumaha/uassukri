5647b02c3030702871ec9a479bc217408b5488c0
sukrianisarumaha <161433305+sukrianisarumaha@users.noreply.github.com>
2024-03-03 05:00:55 +0700

    Add files via upload

---
+++ Pdpj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pdpj;
+
+/**
+ *
+ * @author sukri
+ */
+public class Pdpj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
